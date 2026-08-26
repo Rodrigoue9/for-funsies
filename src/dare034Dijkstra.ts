a158070d00c5f140348552d918f43c8abe461feb
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:15:50 -0300

    feat(algorithms): DARE 034 - Dijkstra shortest path with binary min-heap

---
+++ src/dare034Dijkstra.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 034
+ * feat(algorithms): DARE 034 - Dijkstra shortest path with binary min-heap
+ */
+export function dijkstra(graph: any, start: string) { return { start, distances: {} }; }
