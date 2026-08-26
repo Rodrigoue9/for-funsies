c435e69107becaaa687f8f75b9b3cb12bedbb5bf
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:28 -0300

    feat(graph): DARE 044 - Tarjan Strongly Connected Components (SCC) detector

---
+++ src/dare044TarjanScc.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 044
+ * feat(graph): DARE 044 - Tarjan Strongly Connected Components (SCC) detector
+ */
+export function tarjanScc(graph: any): string[][] { return []; }
