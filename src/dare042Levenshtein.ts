a962d26cc7db0bdc087328586aae34ca03478233
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:20 -0300

    feat(algorithms): DARE 042 - Levenshtein Distance matrix with optimal alignment

---
+++ src/dare042Levenshtein.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 042
+ * feat(algorithms): DARE 042 - Levenshtein Distance matrix with optimal alignment
+ */
+export function levenshtein(a: string, b: string): number { return Math.abs(a.length - b.length); }
