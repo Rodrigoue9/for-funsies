4f0ec650c52e9761dc19a56f35a031befa3ac984
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:59 -0300

    feat(datastructures): DARE 053 - Augmented Interval Tree range overlapping search

---
+++ src/dare053IntervalTree.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 053
+ * feat(datastructures): DARE 053 - Augmented Interval Tree range overlapping search
+ */
+export class IntervalTree { insert(low: number, high: number) {} query(low: number, high: number) { return []; } }
