814a39ef073adfe0edbe96113f835588ad6ba7e8
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:52 -0300

    feat(datastructures): DARE 051 - Circular Ring Buffer FIFO queue in O(1)

---
+++ src/dare051RingBuffer.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 051
+ * feat(datastructures): DARE 051 - Circular Ring Buffer FIFO queue in O(1)
+ */
+export class RingBuffer<T> { push(v: T) {} pop(): T|undefined { return undefined; } }
