46e101053cd48a8b1be4b9f2a086a7b1e3513647
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:12 -0300

    feat(geometry): DARE 040 - 2D Convex Hull Graham Scan polygon builder

---
+++ src/dare040ConvexHull.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 040
+ * feat(geometry): DARE 040 - 2D Convex Hull Graham Scan polygon builder
+ */
+export function convexHull(pts: Array<{x:number, y:number}>) { return pts; }
