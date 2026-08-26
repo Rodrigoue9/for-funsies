9be5869c2923496796df35c9742e99769c36d749
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:05 -0300

    feat(datastructures): DARE 038 - Quadtree 2D spatial indexing and range query

---
+++ src/dare038Quadtree.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 038
+ * feat(datastructures): DARE 038 - Quadtree 2D spatial indexing and range query
+ */
+export class QuadTree { insert(p: any) {} query(range: any) { return []; } }
