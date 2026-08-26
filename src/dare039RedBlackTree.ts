8bbfcbe1f6b9b500c4e764a7027f5dae3c6f6544
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:08 -0300

    feat(datastructures): DARE 039 - Self-balancing Red-Black binary search tree

---
+++ src/dare039RedBlackTree.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 039
+ * feat(datastructures): DARE 039 - Self-balancing Red-Black binary search tree
+ */
+export class RedBlackTree { insert(val: number) {} search(val: number) { return true; } }
