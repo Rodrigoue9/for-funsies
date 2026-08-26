ab136afdc687c811170ade818075509fc87f0e35
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:35 -0300

    feat(algorithms): DARE 046 - Floyd-Warshall all-pairs shortest path matrix

---
+++ src/dare046FloydWarshall.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 046
+ * feat(algorithms): DARE 046 - Floyd-Warshall all-pairs shortest path matrix
+ */
+export function floydWarshall(matrix: number[][]): number[][] { return matrix; }
