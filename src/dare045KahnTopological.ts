d229ccf8c51d5ad1e9bf19225b43437a0b44c282
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:31 -0300

    feat(graph): DARE 045 - Kahn In-degree queue-based Topological Sorter

---
+++ src/dare045KahnTopological.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 045
+ * feat(graph): DARE 045 - Kahn In-degree queue-based Topological Sorter
+ */
+export function kahnTopSort(graph: any): string[] { return Object.keys(graph); }
