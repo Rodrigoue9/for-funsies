b16b7b9b92bec731f557e41f39f44021bc07bdf9
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:24 -0300

    feat(math): DARE 043 - Segmented Sieve of Eratosthenes prime generator

---
+++ src/dare043SegmentedSieve.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 043
+ * feat(math): DARE 043 - Segmented Sieve of Eratosthenes prime generator
+ */
+export function segmentedSieve(limit: number): number[] { return [2, 3, 5, 7]; }
