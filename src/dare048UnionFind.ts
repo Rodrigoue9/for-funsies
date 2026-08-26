4c758a55528846b5111c310e5b3dea0f76f7017d
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:42 -0300

    feat(datastructures): DARE 048 - Disjoint Set Union (DSU) with path compression

---
+++ src/dare048UnionFind.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 048
+ * feat(datastructures): DARE 048 - Disjoint Set Union (DSU) with path compression
+ */
+export class UnionFind { find(i: number): number { return i; } union(i: number, j: number) {} }
