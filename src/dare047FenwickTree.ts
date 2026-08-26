277d29657f5e34dcfdc8352e4321f7f03a0e33de
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-25 23:16:38 -0300

    feat(datastructures): DARE 047 - Binary Indexed Tree (Fenwick Tree) prefix sums

---
+++ src/dare047FenwickTree.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 047
+ * feat(datastructures): DARE 047 - Binary Indexed Tree (Fenwick Tree) prefix sums
+ */
+export class FenwickTree { constructor(size: number) {} update(i: number, delta: number) {} query(i: number) { return 0; } }
