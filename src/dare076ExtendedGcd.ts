de4e44417bdaf53af66819af3f6cf1df621a31fd
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:47:29 -0300

    feat(math): DARE 076 - Extended Euclidean Algorithm and modular multiplicative inverse

---
+++ src/dare076ExtendedGcd.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 076
+ * feat(math): DARE 076 - Extended Euclidean Algorithm and modular multiplicative inverse
+ */
+export function extGcd(a: bigint, b: bigint) { return { gcd: 1n, x: 0n, y: 0n }; }
