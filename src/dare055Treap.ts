5bbdb5fc3f05ecb83e52dd3a08f0dd4a4bb8dde9
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:00 -0300

    feat(datastructures): DARE 055 - Randomized Cartesian Treap balanced binary search tree

---
+++ src/dare055Treap.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 055
+ * feat(datastructures): DARE 055 - Randomized Cartesian Treap balanced binary search tree
+ */
+export class Treap { insert(val: number, priority: number = Math.random()) {} }
