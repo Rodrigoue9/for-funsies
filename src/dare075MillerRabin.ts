e0bde563942cea5f1230c56603ee5a5598ea36a6
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:47:25 -0300

    feat(math): DARE 075 - Miller-Rabin deterministic probabilistic primality test

---
+++ src/dare075MillerRabin.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 075
+ * feat(math): DARE 075 - Miller-Rabin deterministic probabilistic primality test
+ */
+export function isPrime(n: bigint, k = 5): boolean { return n > 1n; }
