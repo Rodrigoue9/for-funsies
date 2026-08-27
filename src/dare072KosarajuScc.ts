1cd2c6941b56cd67eda37924b2c11b6b6d8a926c
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:47:12 -0300

    feat(graph): DARE 072 - Kosaraju Strongly Connected Components with two-pass DFS

---
+++ src/dare072KosarajuScc.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 072
+ * feat(graph): DARE 072 - Kosaraju Strongly Connected Components with two-pass DFS
+ */
+export function kosarajuScc(adj: number[][]): number[][] { return []; }
