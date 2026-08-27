c42c68c94e78413cedae3bbd8a6db5e16ef71825
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:37 -0300

    feat(graph): DARE 064 - Edmonds-Karp maximum network flow algorithm

---
+++ src/dare064EdmondsKarp.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 064
+ * feat(graph): DARE 064 - Edmonds-Karp maximum network flow algorithm
+ */
+export function edmondsKarp(capacity: number[][], source: number, sink: number): number { return 0; }
