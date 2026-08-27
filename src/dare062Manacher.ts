29500ebb12d221270025f32e2cf62214a1d75633
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:30 -0300

    feat(algorithms): DARE 062 - Manacher Algorithm for longest palindromic substring in O(N)

---
+++ src/dare062Manacher.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 062
+ * feat(algorithms): DARE 062 - Manacher Algorithm for longest palindromic substring in O(N)
+ */
+export function longestPalindrome(s: string): string { return s.slice(0, 1); }
