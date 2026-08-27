d6acba7103ce0034f4146173e2f7f66900b907b9
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:47:34 -0300

    feat(math): DARE 077 - Chinese Remainder Theorem (CRT) system of congruences solver

---
+++ src/dare077CrtSolver.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 077
+ * feat(math): DARE 077 - Chinese Remainder Theorem (CRT) system of congruences solver
+ */
+export function solveCrt(num: bigint[], rem: bigint[]): bigint { return 0n; }
