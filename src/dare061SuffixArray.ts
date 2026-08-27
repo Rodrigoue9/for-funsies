99bc752a3509d7e0d4baf1622662b0a429a3cb91
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:26 -0300

    feat(algorithms): DARE 061 - Suffix Array construction algorithm in linear time

---
+++ src/dare061SuffixArray.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 061
+ * feat(algorithms): DARE 061 - Suffix Array construction algorithm in linear time
+ */
+export function buildSuffixArray(text: string): number[] { return [0]; }
