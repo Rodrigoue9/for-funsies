e3e3e64d1e932c46c154507899cc719dd591eacc
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:47:38 -0300

    feat(math): DARE 078 - Karatsuba fast polynomial and big integer multiplication

---
+++ src/dare078Karatsuba.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 078
+ * feat(math): DARE 078 - Karatsuba fast polynomial and big integer multiplication
+ */
+export function karatsubaMultiply(x: bigint, y: bigint): bigint { return x * y; }
