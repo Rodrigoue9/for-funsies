d69785ab63cf657e82ae4989f17b5bfce54baea9
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:45:55 -0300

    feat(algorithms): DARE 054 - Aho-Corasick multiple pattern string matching automaton

---
+++ src/dare054AhoCorasick.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 054
+ * feat(algorithms): DARE 054 - Aho-Corasick multiple pattern string matching automaton
+ */
+export function ahoCorasick(patterns: string[], text: string) { return []; }
