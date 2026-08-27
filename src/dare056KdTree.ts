d08c7d18d0f031e58b17f0891de06bde9481cb14
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:05 -0300

    feat(datastructures): DARE 056 - K-Dimensional Tree (KD-Tree) spatial point partitioning

---
+++ src/dare056KdTree.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 056
+ * feat(datastructures): DARE 056 - K-Dimensional Tree (KD-Tree) spatial point partitioning
+ */
+export class KdTree { constructor(public points: number[][]) {} nearest(p: number[]) { return p; } }
