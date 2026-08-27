b41a6f85b64e32041099e9b1a5cf5fd167efe06e
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:47:08 -0300

    feat(graph): DARE 071 - Tarjan Bridge and Articulation Point detector in undirected graphs

---
+++ src/dare071BridgesDetector.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 071
+ * feat(graph): DARE 071 - Tarjan Bridge and Articulation Point detector in undirected graphs
+ */
+export function findBridges(adj: number[][]): Array<[number, number]> { return []; }
