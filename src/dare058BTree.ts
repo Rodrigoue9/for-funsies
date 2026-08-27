14d83648559f1efe4002dee9f7f2320e877d5c02
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:13 -0300

    feat(datastructures): DARE 058 - B-Tree multi-way search tree indexing structure

---
+++ src/dare058BTree.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 058
+ * feat(datastructures): DARE 058 - B-Tree multi-way search tree indexing structure
+ */
+export class BTree { constructor(public order = 3) {} insert(k: number) {} }
