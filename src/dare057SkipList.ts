c1e09a58a2a94e784fae594e674bee23e3715330
Rodrigoue9 <devhq.oficial@gmail.com>
2026-08-27 13:46:09 -0300

    feat(datastructures): DARE 057 - Probabilistic Skip List with O(log N) search and insert

---
+++ src/dare057SkipList.ts
@@ -0,0 +1,5 @@
+/**
+ * The Dare Relay - DARE 057
+ * feat(datastructures): DARE 057 - Probabilistic Skip List with O(log N) search and insert
+ */
+export class SkipList { insert(v: number) {} search(v: number): boolean { return false; } }
